41740195106a4dc26aad39d3bf4dd858ec851d62
AKVDS <anakaroliny5555@gmail.com>
2025-02-03 12:06:49 +0000

    criei o printf

---
+++ supertrunfo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+
+   printf("DESAFIO SUPER TRUNFO. \n");
+   
+    char Estado;
+    float codigo;
+    float PIB;
+    char pontoturistico;
+
+    printf ("Inserir Estado: \n");
+    scanf("%s", &Estado);
+
+    printf("Inserir código : \n");
+    scanf("%f", &codigo);
+
+    printf ("Inserir PIB: \n");
+    scanf("%f", &PIB);
+
+    printf("Inserir ponto turístico: \n");
+    scanf("%s", &pontoturistico);
+
+    return 0;
+}
